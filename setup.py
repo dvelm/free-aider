96005f626434ed2633564523393e1cc4cbae697e
dvelm <112973030+dvelm@users.noreply.github.com>
2023-08-12 17:53:46 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+import re
+
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+from aider import __version__
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    long_description = re.sub(r"\n!\[.*\]\(.*\)", "", long_description)
+    long_description = re.sub(r"\n- \[.*\]\(.*\)", "", long_description)
+
+setup(
+    name="aider-chat",
+    version=__version__,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "aider = aider.main:main",
+        ],
+    },
+    description="aider is GPT powered coding in your terminal",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/paul-gauthier/aider",
+)
